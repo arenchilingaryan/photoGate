55c139edd93035801896e5ec0a611c7b7c60515a
David_Sevikian <david_sevikian@epam.com>
2021-11-01 23:08:08 +0300

    feat(2): Format webpack configs.

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.tsx'),
-    plugins: [
-        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public/index.html') }),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public/index.html') })],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    alias: {
+      utils: path.resolve(__dirname, 'src/utils/'),
+      shared: path.resolve(__dirname, 'src/components/shared/'),
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.jsx'],
-        alias: {
-            utils: path.resolve(__dirname, 'src/utils/'),
-            shared: path.resolve(__dirname, 'src/components/shared/')
-        },
-    },
+  },
 };
