eba825391d0196f616d3ec5ad8d561b11e225e95
David_Sevikian <david_sevikian@epam.com>
2021-11-12 14:00:05 +0300

    Fix eslint script

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ModuleFederationPlugin } = require('webpack').container
+const { ModuleFederationPlugin } = require('webpack').container;
 
-const deps = require("./package.json").dependencies;
+const deps = require('./package.json').dependencies;
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.tsx'),
   plugins: [
     new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public/index.html') }),
     new ModuleFederationPlugin({
-      name: "Host",
-      filename: "main.bundle.js",
+      name: 'Host',
+      filename: 'main.bundle.js',
       remotes: {
-        layouts: 'Layouts@http://localhost:3001/main.bundle.js'
+        layouts: 'Layouts@http://localhost:3001/main.bundle.js',
       },
       exposes: {},
       shared: {
@@ -19,12 +19,12 @@ module.exports = {
         react: {
           singleton: true,
           requiredVersion: deps.react,
-          eager: true
+          eager: true,
         },
-        "react-dom": {
+        'react-dom': {
           eager: true,
           singleton: true,
-          requiredVersion: deps["react-dom"],
+          requiredVersion: deps['react-dom'],
         },
       },
     }),
@@ -33,13 +33,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[hash:8].js',
     sourceMapFilename: '[name].[hash:8].map',
-    chunkFilename: '[id].[hash:8].js'
+    chunkFilename: '[id].[hash:8].js',
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx'],
     alias: {
       utils: path.resolve(__dirname, 'src/utils/'),
-      shared: path.resolve(__dirname, 'src/components/shared/')
+      shared: path.resolve(__dirname, 'src/components/shared/'),
     },
   },
 };
