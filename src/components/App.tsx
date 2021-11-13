8dcdd015671091f0eb02306ea4996d79c091e119
arenchilingaryan <arenchilingaryan@yandex.ru>
2021-11-13 06:46:19 -0800

    mf

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,126 @@
 import React from 'react';
-import Button from 'shared/button/button';
 import './App.css';
+import ErrorBoundary from '../ErrorBoundary';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+
+const Layouts = React.lazy(() => import('layouts/Layouts'));
+const Auth = React.lazy(() => import('auth/Auth'));
+const Meetings = React.lazy(() => import('meetings/Meetings'));
+const Landing = React.lazy(() => import('landing/Landing'));
+const Messages = React.lazy(() => import('messages/Messages'));
+const Profile = React.lazy(() => import('profile/Profile'));
+const Search = React.lazy(() => import('search/Search'));
+
+const LayoutsComponent = () => (
+  <React.Suspense fallback={<div style={{ background: 'red' }}>Loading...</div>}>
+    <ErrorBoundary>
+      <Layouts />
+    </ErrorBoundary>
+  </React.Suspense>
+);
+const AuthComponent = () => (
+  <React.Suspense fallback={<div style={{ background: 'blue' }}>Loading...</div>}>
+    <ErrorBoundary>
+      <Auth />
+    </ErrorBoundary>
+  </React.Suspense>
+);
+const MeetingsComponent = () => (
+  <React.Suspense fallback={<div style={{ background: 'red' }}>Loading...</div>}>
+    <ErrorBoundary>
+      <Meetings />
+    </ErrorBoundary>
+  </React.Suspense>
+);
+const LandingComponent = () => (
+  <React.Suspense fallback={<div style={{ background: 'red' }}>Loading...</div>}>
+    <ErrorBoundary>
+      <Landing />
+    </ErrorBoundary>
+  </React.Suspense>
+);
+const MessagesComponent = () => (
+  <React.Suspense fallback={<div style={{ background: 'red' }}>Loading...</div>}>
+    <ErrorBoundary>
+      <Messages />
+    </ErrorBoundary>
+  </React.Suspense>
+);
+const ProfileComponent = () => (
+  <React.Suspense fallback={<div style={{ background: 'red' }}>Loading...</div>}>
+    <ErrorBoundary>
+      <Profile />
+    </ErrorBoundary>
+  </React.Suspense>
+);
+const SearchComponent = () => (
+  <React.Suspense fallback={<div style={{ background: 'red' }}>Loading...</div>}>
+    <ErrorBoundary>
+      <Search />
+    </ErrorBoundary>
+  </React.Suspense>
+);
+
+const buttonStyles = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: '#659DBD',
+  borderRadius: '4px',
+  margin: '4px',
+  color: 'white',
+  fontSize: '20px',
+  width: '100px',
+  height: '60px',
+  textDecoration: 'none'
+}
 
 const App = () => {
   return (
-    <div className="div">
-      Zdarove
-      <button role="heading" disabled>
-        Privet
-      </button>
-      <Button />
-    </div>
+    <BrowserRouter>
+      <div style={{display: 'flex', marginTop:'30px'}}>
+        <Link to="/" style={buttonStyles}>host</Link>
+        <Link to="/auth" style={buttonStyles}>auth</Link>
+        <Link to="/layouts" style={buttonStyles}>layouts</Link>
+        <Link to="/meetings" style={buttonStyles}>meetings</Link>
+        <Link to="/landing" style={buttonStyles}>landing</Link>
+        <Link to="/messages" style={buttonStyles}>messages</Link>
+        <Link to="/profile" style={buttonStyles}>profile</Link>
+        <Link to="/search" style={buttonStyles}>search</Link>
+      </div>
+      <div>
+        <React.Suspense fallback={<div>POSHEL NAXYU</div>}>
+          <Switch>
+            <Route exact path="/">
+              <div style={{ fontSize: '100px' }}>
+                HOST
+              </div>
+            </Route>
+            <Route path="/auth">
+              <AuthComponent />
+            </Route>
+            <Route path="/layouts">
+              <LayoutsComponent />
+            </Route>
+            <Route path="/meetings">
+              <MeetingsComponent />
+            </Route>
+            <Route path="/landing">
+              <LandingComponent />
+            </Route>
+            <Route path="/messages">
+              <MessagesComponent />
+            </Route>
+            <Route path="/profile">
+              <ProfileComponent />
+            </Route>
+            <Route path="/search">
+              <SearchComponent />
+            </Route>
+          </Switch>
+        </React.Suspense>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- webpack.common.js
+++ webpack.common.js
@@ -12,21 +12,29 @@ module.exports = {
       filename: 'main.bundle.js',
       remotes: {
         layouts: 'Layouts@http://localhost:3001/main.bundle.js',
+        auth: 'Auth@http://localhost:3002/main.bundle.js',
+        meetings: 'Meetings@http://localhost:3003/main.bundle.js',
+        landing: 'Landing@http://localhost:3004/main.bundle.js',
+        messages: 'Messages@http://localhost:3005/main.bundle.js',
+        profile: 'Profile@http://localhost:3008/main.bundle.js',
+        search: 'Search@http://localhost:3006/main.bundle.js',
       },
       exposes: {},
-      shared: {
+      shared: [{
         ...deps,
         react: {
           singleton: true,
           requiredVersion: deps.react,
-          eager: true,
-        },
+        }
+      },
+      {
         'react-dom': {
-          eager: true,
           singleton: true,
           requiredVersion: deps['react-dom'],
-        },
+        }
       },
+      { "react-router-dom": { requiredVersion: deps["react-router-dom"], singleton: true } }
+      ],
     }),
   ],
   output: {
@@ -34,6 +42,7 @@ module.exports = {
     filename: '[name].[hash:8].js',
     sourceMapFilename: '[name].[hash:8].map',
     chunkFilename: '[id].[hash:8].js',
+    publicPath: '/'
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx'],
